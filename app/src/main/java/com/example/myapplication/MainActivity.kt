fe1a738e668072b450d520e06b1b8469f99522cc
Jhujar Kumar <jhujar.kumar@student.griffith.ie>
2022-11-23 01:08:05 +0000

    Try Camera

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,56 @@
 package com.example.myapplication
 
+import android.app.Activity
+import android.app.DownloadManager.Request
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.FileProvider
+import java.io.File
 
+private const val File_NAME="bReal.jpg"
+private const val Request_CODE=42
+private lateinit var photoFile: File
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnTakePic.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            photoFile = getphotoFile(File_NAME)
+           // takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoFile)
+            val fileProvider = FileProvider.getUriForFile(this,"com.example.fileprovider", photoFile)
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
+            if (takePictureIntent.resolveActivity(this.packageManager) != null) {
+                startActivityForResult(takePictureIntent, Request_CODE)
+            } else {
+                Toast.makeText(this, "Unable to open camera", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
+
+    private fun getphotoFile(fileName: String): File {
+        val storageDirectory=ExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(fileName,".jpg",storageDirectory)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode== Request_CODE && resultCode == Activity.RESULT_OK){
+            val takenImage = BitmapFactory.decodeFile(photoFile.absolutePath)
+            imageView.setImage(takenImage)
+        }else
+        {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+
 }
